99b334040ef1a17a43f2f0d7f236743d190b9cd5
Charles Care <c.p.care@gmail.com>
2010-11-29 17:00:46 +0000

    Improving coverage of ReflectionUtils

--- src/main/java/org/nebularis/defproxy/support/ReflectionUtils.java
+++ src/main/java/org/nebularis/defproxy/support/ReflectionUtils.java
@@ -4,9 +4,7 @@ import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.ClassUtils;
 
 public class ReflectionUtils {
-    public ReflectionUtils() {
-    }
-
+    
     public static boolean isAssignable(final Class<?> expectedReturnType, final Class<?> methodReturnType) {
         if (expectedReturnType == null || methodReturnType == null) {
             return false;
@@ -31,14 +29,14 @@ public class ReflectionUtils {
     }
 
     public static boolean isAssignable(Class[] classArray, Class[] toClassArray) {
-        if (ArrayUtils.isSameLength(classArray, toClassArray) == false) {
-            return false;
-        }
         if (classArray == null) {
             classArray = ArrayUtils.EMPTY_CLASS_ARRAY;
         }
         if (toClassArray == null) {
             toClassArray = ArrayUtils.EMPTY_CLASS_ARRAY;
+        }
+        if (ArrayUtils.isSameLength(classArray, toClassArray) == false) {
+            return false;
         }
         for (int i = 0; i < classArray.length; i++) {
             if (isAssignable(classArray[i], toClassArray[i]) == false) {
